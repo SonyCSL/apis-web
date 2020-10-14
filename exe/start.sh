4d2aca021a92bbef76b55e060e98e8c30032351d
岩瀬俊一郎 <iwaseshunichirou@iwaseshunichirounoMacBook-puro.local>
2020-10-14 23:16:39 +0900

    Change setting_files to exe

---
+++ exe/start.sh
@@ -0,0 +1,5 @@
+echo "call start.sh"
+
+java -XX:OnOutOfMemoryError="'kill -KILL %p'"  -Duser.timezone=Asia/Tokyo -Djava.util.logging.config.file=./logging.properties -jar ../target/apis-web-3.0.0-a01-fat.jar -conf ./config.json -cp ./ -cluster  -cluster-host 127.0.0.1 &
+
+echo "... done"
